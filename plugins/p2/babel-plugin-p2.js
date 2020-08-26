976281dddb2a2f84dc89b514f1263455810b041f
lizhigao <lizhigao@021.com>
2020-08-26 14:28:08 +0800

    update

--- plugins/p2/babel-plugin-p2.js
+++ plugins/p2/babel-plugin-p2.js
@@ -2,8 +2,9 @@ module.exports = function ({ types: t }) {
   return {
     visitor: {
       BinaryExpression(path, state) {
+        // 必须添加if条件判断，否则进入死循环，内存溢出。
         if (t.isBinaryExpression(path.node, { operator: '*' })) {
-          console.log(path.node.left.name);
+          // console.log(path.node.left.name);
           path.replaceWith(t.binaryExpression('**', path.node.left, t.numericLiteral(2)));
         }
       },
